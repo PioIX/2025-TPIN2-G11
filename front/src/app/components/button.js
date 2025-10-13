0da2702498723e4d7f6099b25be447b165067c68
franpicc5 <fpicca@pioix.edu.ar>
2025-10-13 08:46:51 -0300

    página creada en next.js

---
+++ front/src/app/components/button.js
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Button({ title, ...props }) {
+  return (
+    <button {...props}>
+      {title}
+    </button>
+  );
+}
---
+++ front/src/app/page.js
@@ -0,0 +1,12 @@
+import Button from "./components/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <Button title="CREAR SALA"/>
+    <Button title="UNIRME A SALA"/>
+    <Button title="VER RANKING"/>
+    </>
+  );
+}
