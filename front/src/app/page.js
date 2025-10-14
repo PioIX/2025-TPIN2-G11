ea6563989bfd0817163c117ff389175acc4c86af
franpicc5 <fpicca@pioix.edu.ar>
2025-10-14 08:42:41 -0300

    boton de configuración

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -4,9 +4,12 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-    <Button title="CREAR SALA"/>
-    <Button title="UNIRME A SALA"/>
-    <Button title="VER RANKING"/>
+      <a onClick={}>
+        <img src="" alt="Configuración"/>
+      </a>
+      <Button title="CREAR SALA" />
+      <Button title="UNIRME A SALA" />
+      <Button title="VER RANKING" />
     </>
   );
 }
