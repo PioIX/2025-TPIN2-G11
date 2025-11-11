f8863914f205b3274f566a5c043a54ee47cf2aeb
Francisco Picca <fpicca@pioix.edu.ar>
2025-11-11 18:23:28 -0300

    randomRole terminado y hecho componente

--- front/src/components/randomRole.js
+++ front/src/components/randomRole.js
@@ -1,7 +1,46 @@
-export default function RandomRole({ playersAmount }) {
-    let players=["pepito2", "etesech", "peroxido", "XxjuancitoGaymerxX", "vusconobia", "permatrago"]
-    for (let i = playersAmount - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        console.log()
+export default function RandomRole({ array }) {
+    const shuffledArray = [...array];
+    let currentIndex = shuffledArray.length;
+
+    // algoritmo Fisher-Yates
+    while (currentIndex !== 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
+            shuffledArray[randomIndex],
+            shuffledArray[currentIndex],
+        ];
     }
+
+    const roles = [
+        "Palermitano", "Conurbanense", "Conurbanense", "Medium",
+        "Tarotista", "Lobizón", "Palermitano", "Lobizón",
+        "Viuda negra", "Random1", "Conurbanense", "Lobizón",
+        "Palermitano", "Random2", "Conurbanense", "Palermitano"
+    ];
+
+    const randomPool = ["Pombero", "Jubilado", "Chamán"];
+    const usedRandomRoles = [];
+
+    // Asignar roles
+    const result = shuffledArray.map((item, i) => {
+
+        let role = roles[i];
+        if (role === "Random1" || role === "Random2") {
+            if (roles.length > 13) {
+                randomPool.push("Colectivero");
+            }
+            //rol aleatorio
+            const randomIndex = Math.floor(Math.random() * randomPool.length);
+            role = randomPool[randomIndex];
+            usedRandomRoles.push(role);
+            randomPool.splice(randomIndex, 1);
+
+
+        }
+
+        return `${item}: ${role}`;
+    });
+
+    return result;
 }
